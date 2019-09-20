3d88e673ab4bad30a1f88bf06d08f718f1428413
Đỗ Thanh Tân <thanhtan28011998@users.noreply.github.com>
2019-09-20 21:11:16 +0700

    CSharp
    Form

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace DoThanhTan
+{
+    partial class Pictures
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.linkavartar = new System.Windows.Forms.LinkLabel();
+            this.picture = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // linkavartar
+            // 
+            this.linkavartar.AutoSize = true;
+            this.linkavartar.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkavartar.Location = new System.Drawing.Point(400, 379);
+            this.linkavartar.Name = "linkavartar";
+            this.linkavartar.Size = new System.Drawing.Size(112, 19);
+            this.linkavartar.TabIndex = 1;
+            this.linkavartar.TabStop = true;
+            this.linkavartar.Text = "Choose image";
+            this.linkavartar.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkavartar_LinkClicked);
+            // 
+            // picture
+            // 
+            this.picture.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picture.Image = global::DoThanhTan.Properties.Resources.anh_bia_facebook_hacker_anonymous_kem_status_doc_dao_9;
+            this.picture.Location = new System.Drawing.Point(24, 12);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(859, 308);
+            this.picture.TabIndex = 0;
+            this.picture.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Pictures
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(912, 450);
+            this.Controls.Add(this.linkavartar);
+            this.Controls.Add(this.picture);
+            this.Name = "Pictures";
+            this.Text = "Thông tin về ảnh";
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picture;
+        private System.Windows.Forms.LinkLabel linkavartar;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoThanhTan
+{
+    public partial class Pictures : Form
+    {
+        Image image;
+        string openFileName;
+        string pathDirectoryImg;
+        List<string> imageFiles = new List<string>();
+        int index = 1;
+        public Pictures()
+        {
+            InitializeComponent();
+            picture.AllowDrop = true;
+            pathDirectoryImg = Application.StartupPath + "/img";
+            if (Directory.Exists(Application.StartupPath + "/img"))
+            {
+                var files = Directory.GetFiles(Application.StartupPath + "/img", "*.*", SearchOption.AllDirectories);
+                if (files.Length > 0)
+                {
+                    foreach (string filename in files)
+                    {
+                        if (Regex.IsMatch(filename, @".jpg|.png|.gif$"))
+                        {
+                            imageFiles.Add(filename);
+                        }
+                    }
+                    FileStream fileStream = new FileStream(imageFiles[0], FileMode.Open, FileAccess.Read);
+                    picture.Image = Image.FromStream(fileStream);
+                    fileStream.Close();
+                }
+
+            }
+            else
+            {
+                picture.Image = Properties.Resources.design_form_SV;
+            }
+        }
+
+        private void linkavartar_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Choose image";
+            openFileDialog.Filter = "File image(*.png, *.jpg)|*.png;*.jpg";
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                openFileName = openFileDialog.FileName;
+                try
+                {
+                    var files = Directory.GetFiles(folderDialog.SelectedPath, "*.*", SearchOption.AllDirectories);
+                    imageFiles = new List<string>();
+                    int count = 1;
+                    foreach (string filename in files)
+                    {
+                        if (Regex.IsMatch(filename, @".jpg|.png|.gif$"))
+                        {                        
+                            imageFiles.Add(filename);                        
+                        }
+                    }
+                    if (imageFiles.Count > 0)
+                    {
+                        timer.Stop();
+                        if (Directory.Exists(Application.StartupPath + "/img"))
+                        {
+                            var old_files = Directory.GetFiles(Application.StartupPath + "/img", "*.*", SearchOption.AllDirectories);
+                            if (old_files.Length > 0)
+                            {
+                                foreach (string filename in old_files)
+                                {
+                                    if (Regex.IsMatch(filename, @".jpg|.png|.gif$"))
+                                    {
+
+                                        File.Delete(filename);
+                                    }
+                                }
+                            }
+                        }
+
+                        foreach (string filename in imageFiles)
+                        {
+                            image = Image.FromFile(filename);
+                            if (image != null)
+                            {
+                                if (!Directory.Exists(pathDirectoryImg))
+                                {
+                                    Directory.CreateDirectory(pathDirectoryImg);
+                                }
+
+                                string st = String.Format("/slide_{0}.png", count);
+                                image.Save(pathDirectoryImg + st);
+                                count++;
+
+                            }
+                        }
+
+                    }
+
+                    MessageBox.Show("Upload successful. New picture will upload after 3s",
+                                "Notification",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+                    index = 0;
+                    timer.Start();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("An error occurred while attempting to upload the file. The error is:"
+                                    + System.Environment.NewLine + ex.ToString() + System.Environment.NewLine);
+                }
+                Invalidate();
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (imageFiles.Count > 0)
+            {
+                FileStream fileStream = new FileStream(imageFiles[index], FileMode.Open, FileAccess.Read);
+                picture.Image = Image.FromStream(fileStream);
+                fileStream.Close();
+                if (index == imageFiles.Count - 1)
+                    index = 0;
+                else
+                    index++;
+            }
+        }
+    }
+}
